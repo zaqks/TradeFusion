61d50dd608a19f92ccd2ef149dd776c4a15b5c1c
zak <imzakbro@gmail.com>
2024-11-01 04:29:51 +0100

    stocks

--- api/stocks_app/views.py
+++ api/stocks_app/views.py
@@ -6,5 +6,16 @@ from .models import Stock
 
 def stocks(request):
     stks = Stock.objects.all()
-    
+
     return render(request, "stocks_app/stocks.html", {"stks":stks})
+
+
+
+def imports(request):
+    return render(request, "stocks_app/imports.html", {"stks":stks})
+
+
+
+def exports(request):
+    return render(request, "stocks_app/exports.html", {"stks":stks})
+    

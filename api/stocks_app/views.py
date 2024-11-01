8562e584f3f5c542dc7b13ea026d84f866b691db
zak <imzakbro@gmail.com>
2024-11-01 03:41:40 +0100

    stks

--- api/stocks_app/views.py
+++ api/stocks_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Stock
 # Create your views here.
 
 
 
 def stocks(request):
-    return render(request, "stocks_app/stocks.html", {})
+    stks = Stock.objects.all()
+    
+    return render(request, "stocks_app/stocks.html", {"stks":stks})

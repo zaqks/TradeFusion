f70612a1b4239872bd0aab2a86122f4052d661ba
zak <imzakbro@gmail.com>
2024-11-01 07:34:01 +0100

    imps

--- api/stocks_app/views.py
+++ api/stocks_app/views.py
@@ -12,12 +12,16 @@ def stocks(request):
 
 
 def imports(request):
-    return render(request, "stocks_app/imports.html", {"stks":None})
+    stks = Stock.objects.all()
+
+    return render(request, "stocks_app/imports.html", {"stks":stks})
 
 
 
 def exports(request):
-    return render(request, "stocks_app/exports.html", {"stks":None})
+    stks = Stock.objects.all()
+
+    return render(request, "stocks_app/exports.html", {"stks":stks})
 
 
 def stock(request, pk):

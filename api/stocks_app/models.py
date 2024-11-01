78d7f2690a3b09f257bed9e3a2a7e064df05879f
zak <imzakbro@gmail.com>
2024-11-01 01:09:17 +0100

    ui

--- api/stocks_app/models.py
+++ api/stocks_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=50)
+    country_of_origin = models.CharField(max_length=100)
+    exported_to = models.JSONField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_value = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name

205c932cf000ecc304d89d4b009d0b4f8cebc842
zak <imzakbro@gmail.com>
2024-11-01 02:45:25 +0100

    auth

--- api/_main_app/settings.py
+++ api/_main_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hnzdb!jq8gzd7mr)u$ww$+a13^hnu)vpz31u!^e**8c9@^fzx4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,8 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = f"{BASE_DIR}/staticfiles"
+
+STATICFILES_DIRS = (f"{BASE_DIR}/templates",)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+

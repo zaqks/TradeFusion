d92011a88aef12a12e92962f9bae865e29554baf
zak <imzakbro@gmail.com>
2024-11-01 04:36:43 +0100

    imp_exp

--- api/stocks_app/urls.py
+++ api/stocks_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import stocks
+from .views import stocks, imports, exports
 
 app_name = "stocks_app"
 
 urlpatterns  =[
-    path("", stocks, name="stocks")
+    path("", stocks, name="stocks"),
+    path("imports", imports, name="imports"),
+    path("exports", exports, name="exports")
 ]
